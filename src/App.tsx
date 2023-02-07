c72acc809523cc38b33f582035324a3f97484e78
alexiaestefania <alexiaestefania666@gmail.com>
2023-02-07 11:18:23 -0300

    Initial working state

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import axios from 'axios';
+import dayjs from "dayjs";
 import { useState } from 'react';
 import styles from './App.module.scss';
-import searchsvg from "./assets/search.svg"
-import idle from "./assets/searchlarge.svg"
 import chevron from "./assets/chevron-right.svg";
+import searchsvg from "./assets/search.svg";
+import idle from "./assets/searchlarge.svg";
 import cardstyles from './cards.module.scss';
+import { UserDataRes } from "./types/types";
+import colorsJson from "./assets/github-lang-colors.json"
 
-type githubresponse = {
-  avatar_url: string;
-  bio: string;
-  name: string;
-  username: string;
-  followers: string;
-  following: string;
-  repos_url: string;
-  public_repos: string;
+function joinedDate(ISO: string): string {
+  const date = dayjs(ISO);
+  const formatDate = `Ingressou em ${date.format("DD MMM YYYY")}`
+  return formatDate;
 }
+
+function CreatedDate(ISO: string): string {
+  const date = dayjs(ISO);
+  const formatDate = `Criado em ${date.format("DD MMM YYYY")}`
+  return formatDate;
+}
+
+function UpdatedDate(ISO: string): string {
+  const date = dayjs(ISO);
+  const formatDate = `Atualizado em ${date.format("DD MMM YYYY")}`
+  return formatDate;
+}
+
+
 
 function App() {
   const [username, setUsername] = useState("");
@@ -26,14 +38,18 @@ function App() {
   const [followers, setFollowers] = useState("Aguardando");
   const [following, setFollowing] = useState("Aguardando");
   const [public_repos, setPublicrepos] = useState("Aguardando");
-  const [repos, setRepos] = useState("Aguardando");
   const [showResults, setShowResults] = useState(false);
   const [showDetail, setShowDetail] = useState(false);
-
+  const [joindate, setjoindate] = useState("Aguardando");
+  const [githublink, setgithublink] = useState("Aguardando");
+  const [repos, setRepos] = useState();
+  
   const usernamesearch = () => {
+    
     setShowDetail(false);
+
     axios
-    .get<githubresponse>(`http://api.github.com/users/${username}`)
+    .get<UserDataRes>(`http://api.github.com/users/${username}`)
     .then((res) => {
       setName(res.data.name);
       setHandle(username);
@@ -41,16 +57,34 @@ function App() {
       setAvatarUrl(res.data.avatar_url);
       setFollowers(res.data.followers);
       setFollowing(res.data.following);
+      setgithublink(res.data.html_url);
       setPublicrepos(res.data.public_repos);
-      setRepos(res.data.repos_url);
-      setShowResults(true)
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
+      setjoindate(joinedDate(res.data.created_at));
+      setShowResults(true);
     });
-  };
 
+    axios
+    .get(`http://api.github.com/users/${username}/repos`)
+    .then((res) => {
+      const list = res.data.map((item : any) => (
+        <a href={item.html_url}>
+          <div key={item.id} className={cardstyles.RepoCard}> 
+            <a href={item.html_url}>{item.name}</a>
+            <span className={cardstyles.Description}>{item.description}</span>
+              <span className={cardstyles.LangDot}></span>
+              <span className={cardstyles.Language}> {item.language}</span>
+              <div className={cardstyles.RepoDaterapper}>
+                <p>{CreatedDate(item.created_at)}</p>
+                <p>{UpdatedDate(item.updated_at)}</p>
+              
+            </div>
+          </div>
+        </a>
+      ));
+      setRepos(list)
+      });   
+
+  }
 
   const Results = () => (
     <div id="results" className={styles.usernamelist}>
@@ -75,44 +109,39 @@ function App() {
   const UserDetail = () => (
     <div id="detailresult"> 
       <h3>Detalhes do Perfil</h3>
-      <div className={cardstyles.UserDetail}>
-        <img className={cardstyles.Avatar} src={avatar_url} alt="User Avatar" />
-        <div className={cardstyles.Userdata}>
-          <h1>{name}</h1>
-          <h2>@{handle}</h2>
-          <span>{bio}</span>
-          <span>Join Date</span>
-          <div className={cardstyles.UserStats}>
-            <div className={cardstyles.StatsGridH}>Repositórios</div>
-            <div className={cardstyles.StatsGridH}>Seguidores</div>
-            <div className={cardstyles.StatsGridH}>Seguindo</div>
-            <div className={cardstyles.StatsGridItem}>{public_repos}</div>
-            <div className={cardstyles.StatsGridItem}>{followers}</div>
-            <div className={cardstyles.StatsGridItem}>{following}</div>
+      <a href={githublink}>
+        <div className={cardstyles.UserDetail}>
+          <img className={cardstyles.Avatar} src={avatar_url} alt="User Avatar" />
+          <div className={cardstyles.Userdata}>
+            <span className={cardstyles.joindate}>{joindate}</span>
+            <h1>{name}</h1>
+            <h2>@{handle}</h2>
+            <span>{bio}</span>
+            
+            <div className={cardstyles.UserStats}>
+              <div className={cardstyles.StatsGridH}>Repositórios</div>
+              <div className={cardstyles.StatsGridH}>Seguidores</div>
+              <div className={cardstyles.StatsGridH}>Seguindo</div>
+              <div className={cardstyles.StatsGridItem}>{public_repos}</div>
+              <div className={cardstyles.StatsGridItem}>{followers}</div>
+              <div className={cardstyles.StatsGridItem}>{following}</div>
+              </div>
             </div>
-          </div>
-      </div>
+        </div>
+      </a>
       
       <h3>Repositórios</h3>
-
-      <div className={cardstyles.ReposList}>
-
-        </div>
+      <div className={cardstyles.UserRepos}>
+        {repos}
       </div>
-        
-
-
-      </div>
-
-      
     </div>
+    
   );
 
 
   return (
     <div className="App">
       <div className={styles.sidebar}>
-
         <h1>Encontrar Dev</h1>  
         <div className={styles.searchbox}>
           <img src={searchsvg} alt="icone de busca"/>
@@ -129,15 +158,11 @@ function App() {
               Buscar
             </button>
         </div>
-        
         { showResults ? <Results /> : null }
-
       </div>
 
       <div className={styles.MainContainer}>
-
         { showDetail ? <UserDetail /> : <Loading /> }
-
       </div>
     </div>
   );
---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+export type UserDataRes = {
+    avatar_url: string;
+    bio: string;
+    name: string;
+    username: string;
+    followers: string;
+    following: string;
+    public_repos: string;
+    created_at: string;
+    html_url: string;
+  }
